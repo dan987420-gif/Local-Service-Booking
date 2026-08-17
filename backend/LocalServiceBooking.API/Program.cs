079e4955ab88a020c036fec6201c93ae5ccea5b0
dan987420 <dan987420@gmail.com>
2026-08-17 08:22:16 +0530

    Allow all origins in backend CORS policy to support deployed frontend

--- backend/LocalServiceBooking.API/Program.cs
+++ backend/LocalServiceBooking.API/Program.cs
@@ -151,10 +151,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins(allowedOrigins)
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
